d5bd7f47e44211ab316614c88882f6c85c141829
Sahobisoa Rolland ANDRIANIAINA <asahobisoa@isasolutions.mg>
2023-01-09 18:03:34 +0300

    structure 0.1

---
+++ Sah/sah/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Home = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img width={200} src="https://scontent.ftnr1-1.fna.fbcdn.net/v/t39.30808-6/307505030_1243786169792658_2170599009470723363_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGs-m44RYVBnOyLMsfxa5kCKIBu7eOECCwogG7t44QILOZ6TupruzrRlC8er5ka3ZibZytweGtLU07VlRwS853J&_nc_ohc=qOnP-B5kVEcAX-MRfoZ&_nc_ht=scontent.ftnr1-1.fna&oh=00_AfARTOiWEiDZMpIuHxCtaoH_4tq_3GcrAwwHupzCRYo8Jg&oe=63C1423A" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.facebook.com/photo/?fbid=1243786176459324&set=a.112004602970826"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Meet Sahobisoa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
