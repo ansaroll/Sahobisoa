2ad04ecee3af7a0e2fe57a3958226f481a857778
Ansaroll <andrianiainarolland123@gmail.com>
2023-01-13 00:39:02 +0300

    error 404 page

--- Sah/sah/src/components/Error404.tsx
+++ Sah/sah/src/components/Error404.tsx
@@ -1,17 +1,49 @@
-import { Typography } from "@mui/material";
-import Button from '@mui/material/Button';
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
-const Error = () => {
-
+const Error404 = () => {
   return (
-    <>
-      <Typography>Error 404: page not found</Typography>
-      <Button>
-        <Link to={'/'}>Go back to home</Link>
-      </Button>
-    </>
-  )
+    <Box
+      style={{
+        color: "#438AFE",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "2rem",
+      }}
+    >
+      <Typography variant="h5">Oups...</Typography>
+      <Box
+        style={{
+          color: "#438AFE",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src="/assets/404.svg"
+          alt="404"
+          style={{ width: "50%", margin: "auto" }}
+        />
+      </Box>
+      <Box>
+        <Typography variant="h5">
+          Il semblerait qu’il y ait un problème
+        </Typography>
+      </Box>
+      <Link to={"/"} style={{
+        textDecoration:"none"
+      }}>
+        <Button variant="contained" color="primary">
+          Accueil
+        </Button>
+      </Link>
+    </Box>
+  );
 };
 
-export default Error;
+export default Error404;
--- Sah/sah/src/components/Header.tsx
+++ Sah/sah/src/components/Header.tsx
@@ -13,7 +13,6 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import { ThemeProvider } from '@mui/material/styles';
 
 
 const pages = [
