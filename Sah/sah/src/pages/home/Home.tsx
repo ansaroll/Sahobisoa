105674b3329fe2856bbe48eae10faa396d436d90
Ansaroll <andrianiainarolland123@gmail.com>
2023-01-31 22:04:45 +0300

    fix ui mobile /home

--- Sah/sah/src/pages/home/Home.tsx
+++ Sah/sah/src/pages/home/Home.tsx
@@ -1,17 +1,44 @@
-import React, { useContext } from 'react';
-import './homePage.css';
+import React, { useContext } from "react";
+import { Grid, Theme, Typography, useMediaQuery } from "@mui/material";
 
 const Home = () => {
+
+  const isSmorDown = useMediaQuery((theme:Theme) => theme.breakpoints.down('sm'))
+
   return (
-    <div className="home-page">
-      <div className="left-section">
-        <p className="centered-text"> Repérez vos besoins, on s’occupe du reste, avec les meilleurs talents</p>
-      </div>
-      <div className="right-section">
-        <img src={'/assets/illustration.svg'} alt="Illustration" className="centered-image" height="480px" />
-      </div>
-    </div>
+    <Grid container overflow="hidden" py={2}>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant={isSmorDown ? 'h3' :"h2"} textAlign="center">
+          Repérez vos besoins, on s’occupe du reste, avec les meilleurs talents
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        py={{xs:4,sm:2}}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <img
+          width="100%"
+          src={"/assets/illustration.svg"}
+          alt="Illustration"
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default Home;
--- Sah/sah/src/pages/profil/ProfilePage.tsx
+++ Sah/sah/src/pages/profil/ProfilePage.tsx
@@ -22,6 +22,7 @@ const ProfilePage = (props: Props) => {
           }}
         />
       </Grid>
+      
       <Grid
         item
         xs={12}
